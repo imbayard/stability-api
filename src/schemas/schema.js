ce8def21087c58f58dd225566469aafd4af73297
imbayard <beton@bu.edu>
2023-03-06 19:44:11 -0500

    sample data

--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -1,13 +1,71 @@
 const { gql } = require('apollo-server-lambda');
 
 module.exports.typeDefs = gql`
-  type Orders {
-    id: String!
-    amount: Float!
-    tax: Float!
-    total: Float!
-  }
-  type Query {
-    getOrders: [Orders]
-  }
+
+    type User {
+        id: String!,
+        name: String!,
+        currentBalanceProfile: BalanceProfile!,
+        actions: [Action]!,
+        week: Week!,
+        weekReports: [Week]!,
+        day: Day!,
+        dayReports: [Day]!,
+        preferences: Preferences
+    }
+
+    type BalanceProfile {
+        body: Float!,
+        mind: Float!
+    }
+
+    type Action {
+        name: String!,
+        points: Int!,
+        category: String!,
+        timesSet: Int!,
+        timesCompleted: Int!,
+        completedTimeline: [Int]!
+        createdDate: String!,
+        deletedDate: String,
+        active: Boolean!
+    }
+
+    type Week {
+        pointsSet: Int!,
+        pointsComplete: Int!,
+        actionsComplete: [ActionImpl]!
+        startDate: String!,
+        endDate: String!
+    }
+
+    type ActionImpl {
+        name: String!,
+        points: Int!
+    }
+
+    type Day {
+        pointsSet: Int!,
+        pointsComplete: Int!,
+        actionsComplete: [ActionImpl]!
+        date: String!
+    }
+
+    type Preferences {
+        intensitySchedule: IntensitySchedule!
+    }
+
+    type IntensitySchedule {
+        monday: Int!,
+        tuesday: Int!,
+        wednesday: Int!,
+        thursday: Int!,
+        friday: Int!,
+        saturday: Int!,
+        sunday: Int!
+    }
+
+    type Query {
+        userInfo(id: String!): User
+    }
 `
